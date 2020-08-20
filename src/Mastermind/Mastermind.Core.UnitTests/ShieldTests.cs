c0ce6f717a1e0048e50248e37684d29bba07262b
Luís Amorim <luisgomesamorim@gmail.com>
2020-08-20 22:14:51 +0100

    Added unit tests to test for color index match and also exceptions thrown on instantiation.

--- src/Mastermind/Mastermind.Core.UnitTests/ShieldTests.cs
+++ src/Mastermind/Mastermind.Core.UnitTests/ShieldTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Mastermind.Core.UnitTests
@@ -47,6 +48,53 @@ namespace Mastermind.Core.UnitTests
 
             //Assert
             shield.Count.Should().Be(count);
+        }
+
+        [Fact]
+        public void ShouldMatchHolesIndexesWhenShieldHasColors()
+        {
+            //Arrange
+            var colors = new[]
+            {
+                CodePeg.Black,
+                CodePeg.Blue,
+                CodePeg.Green,
+                CodePeg.White
+            };
+
+            //Act
+            var shield = new Shield(colors);
+
+            //Assert
+            shield[0].Should().Be(CodePeg.Black);
+            shield[1].Should().Be(CodePeg.Blue);
+            shield[2].Should().Be(CodePeg.Green);
+            shield[3].Should().Be(CodePeg.White);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentExceptionWhenColorsIsEmpty()
+        {
+            //Arrange
+            var colors = new CodePeg[0];
+
+            //Act
+            var action = new Action(() => new Shield(colors));
+
+            //Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenColorsIsNull()
+        {
+            //Arrange
+
+            //Act
+            var action = new Action(() => new Shield(null));
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>();
         }
     }
 }
